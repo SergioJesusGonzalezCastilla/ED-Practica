f897eb5f10e1e9b5df9bb52749c9f2d9f833dfd4
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2025-05-09 10:41:30 +0200

    Main del programa

---
+++ AplicationMemory/src/aplicationmemory/AplicationMemory.java
@@ -0,0 +1,19 @@
+
+package aplicationmemory;
+
+/**
+ *
+ * @author Sergio Jesus González
+ * @author Alejandro Serrejón
+ */
+public class AplicationMemory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Dentro del main, localizamos todas las llamadas necesarias
+        
+    }
+    
+}
