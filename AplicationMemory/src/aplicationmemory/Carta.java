1a5ca187fe598542a6842b81adfc5d891e3e5cbb
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2025-05-12 11:25:44 +0200

    Update Carta.java

--- AplicationMemory/src/aplicationmemory/Carta.java
+++ AplicationMemory/src/aplicationmemory/Carta.java
@@ -1,55 +1,58 @@
-
-
+//paquete de la aplicación
 package aplicationmemory;
 
-import java.util.*;
 /**
- *
  * @author Sergio Jesus González
  * @author Alejandro Serrejón
  */
 
+//Declaración de la clase
 public class Carta {
+    
+    //Datos:    
     String nombre;
     String pareja;
     boolean descubierta;
     
+    //*Métodos:
+    
+    //Método constructor
     public Carta(String nombre, String pareja){
         this.pareja = pareja;
         this.nombre = nombre;
         this.descubierta = false;
     }
     
+    //*Métodos getters:
     public String getNombre(){
         return nombre;
     }
+    
     public String getPareja(){
         return pareja;
     }
+    
     public Boolean isDescubierta(){
         return descubierta;
     }
-    
+
+    //*Métodos setters:
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
+    
     public void setPareja(String pareja){
         this.pareja = pareja;
     }
+    
     public void setDescubierta(Boolean descubierta){
         this.descubierta = descubierta;
     }
     
-    
+   //Se sobreescribe el método toString()
     @Override
     public String toString(){
         return this.nombre;
     }
-    /*@Override
-    public boolean equals (Object carta){
-        if(this == carta){
-            return true;
-        }
-        return false;
-    }*/
 }
+
