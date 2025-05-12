12cd81e15e30b294801964684d15a3fd9c0fa83f
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2025-05-12 11:29:24 +0200

    Update Jugador.java

--- AplicationMemory/src/aplicationmemory/Jugador.java
+++ AplicationMemory/src/aplicationmemory/Jugador.java
@@ -1,36 +1,44 @@
-
+//paquete de la aplicación
 package aplicationmemory;
 
-import java.util.*;
-
 /**
- *
  * @author Sergio Jesus González
  * @author Alejandro Serrejón
  */
 
-public class Jugador<E>{
-    private String nombre;
-    private Lista<Carta> cartasGanadas;
+//Declaración de la clase
+public class Jugador{
     
+    //Datos:
+    private final String nombre; //nombre
+    private final Lista<Carta> cartasGanadas; //cartas que ha ganado
+    
+    //*Métodos:
+    
+    //Método constructor
     public Jugador(String nombre){
         this.nombre = nombre;
         cartasGanadas = new Lista<Carta>();
     }
     
+    //*Métodos getters:
     public String getNombre(){
         return nombre;
     }
+    
     public Lista<Carta> getCartasGanadas(){
         return cartasGanadas;
     }
     
+    //Añade una carta a la lista de cartas   
     public void anadirCartas (Carta carta){
         cartasGanadas.add(carta);
     }
     
+    //devuelve número de cartas ganadas
     public int size(){
         return cartasGanadas.size();
     }
     
 }
+
