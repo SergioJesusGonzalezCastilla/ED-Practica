fc32bed0efebf8a1993208cf95601fa7edd9663e
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2025-05-12 11:35:43 +0200

    Update Tablero.java

--- AplicationMemory/src/aplicationmemory/Tablero.java
+++ AplicationMemory/src/aplicationmemory/Tablero.java
@@ -1,25 +1,29 @@
-
+//paquete de la aplicación
 package aplicationmemory;
 
-import java.util.*;
-
 /**
- *
  * @author Sergio Jesus González
  * @author Alejandro Serrejón
  */
 
+//Declaración de la clase
 public class Tablero {
+    //Datos:
     private int numColumnas;
-    private int numFilas;
-    private Carta[][] tablero;
+    private int numFilas; //dimensiones del tablero
+    private Carta[][] tablero; //matriz que contiene las cartas
     
+    //*Métodos:
+    
+    //Método constructor
+    //crea el tablero (esta vacío)
     public Tablero(int numFilas, int numColumnas){
         this.numColumnas = numColumnas;
         this.numFilas = numFilas;
         this.tablero = new Carta[numFilas][numColumnas];
     }
     
+    //Llena el tablero con cartas de la baraja
     public void inicializarTablero (Baraja baraja){
         for(int i = 0; i<numFilas; i++){
             for (int j=0; j<numColumnas; j++){
@@ -28,7 +32,7 @@ public class Tablero {
         }
     }
     
-    
+    //Devuelve la carta en la posición especificada
     public Carta getCarta(int fila, int columna) throws IllegalArgumentException {
         if (fila >= 0 && fila < numFilas && columna >= 0 && columna < numColumnas) {
             return tablero[fila][columna];
@@ -36,6 +40,13 @@ public class Tablero {
         throw new IllegalArgumentException("Posición fuera de los límites del tablero");
     }
     
+    //1. Obtiene la carta seleccionada
+    //2. La marca como descubierta
+    //3. La añade al array de cartas seleccionadas
+    //4. Si es la segunda carta seleccionada:
+    //- Compara si forman pareja
+    //- Si coinciden, las asigna al jugador
+    //- Si no, las vuelve a ocultar
     public void seleccionarCarta(int fila, int columna, Carta[] cartasSeleccionadas,int numCartas, Jugador[] jugadores, int jugadorActual ){
         Carta carta = tablero[fila][columna];
         carta.setDescubierta(true);
@@ -58,7 +69,7 @@ public class Tablero {
         }
     }   
    
-    
+    //muestra el tablero, las ocultasse representan con "?"
     public void mostrarTablero (){
         for(int i = 0; i < numFilas; i++){
             for(int j = 0; j < numColumnas; j++){
@@ -69,7 +80,7 @@ public class Tablero {
         }
     }
     
-    
+    //verifica si el juego ha terminado
     public boolean todoDescubierto() {
         for (int i = 0; i < numFilas; i++) {
             for (int j = 0; j < numColumnas; j++) {
@@ -81,7 +92,4 @@ public class Tablero {
         return true;
     }
     
-    
-    
-
 }
